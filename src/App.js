73e358bf51f07fe5f764097492537a350ce7bb5d
mico1784 <mico123123@naver.com>
2024-07-24 17:23:35 +0900

    세션만들기기

--- src/App.js
+++ src/App.js
@@ -13,11 +13,16 @@ import Channel from './pages/Channel'
 import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
+    <Main>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/today' element={<Today/>}/>
@@ -32,6 +37,8 @@ const App = () => {
       <Route path='/search/:searchID' element={<Search/>}/>
       <Route path='/*' element={<Not/>}/>
     </Routes>
+    </Main>
+    <Footer/>
     </BrowserRouter>
   )
 }
